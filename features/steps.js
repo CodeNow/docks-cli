925e3d59db8a94412000c9559b8d39dba23d29aa
Bryan Kendall <bryan@runnable.com>
2016-04-05 15:23:55 -0700

    extend timeout

--- features/steps.js
+++ features/steps.js
@@ -7,7 +7,7 @@ var execFile = Promise.promisify(require('child_process').execFile, { multiArgs:
 var executablePath = path.join(__dirname, '..', 'bin', 'docks')
 
 module.exports = function () {
-  this.When(/^I run `docks (.+)`$/, function (args) {
+  this.When(/^I run `docks (.+)`$/, { timeout: 10 * 1000 }, function (args) {
     this.lastRun = {}
     return execFile(executablePath, args.split(' '))
       .bind(this)
